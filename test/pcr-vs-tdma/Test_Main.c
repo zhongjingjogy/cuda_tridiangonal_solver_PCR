a210e03669c41d8671edd52f5421c8504aae015b
zhongjingjogy <zhongjingjogy@gmail.com>
2018-12-08 11:55:32 +0800

    update: refactor the pcr test.

--- test/pcr-vs-tdma/Test_Main.c
+++ test/pcr-vs-tdma/Test_Main.c
@@ -6,78 +6,7 @@
 #include <iostream>
 #include "pcr/PCR_Class.h"
 #include "tdma/tdma.h"
-
-double maxarr(double *arr, int Nx) {
-    double v = arr[0];
-    for (int nx = 0; nx < Nx; nx++) {
-        if (v < arr[nx]) {
-            v = arr[nx];
-        }
-    }
-    return v;
-}
-
-double minarr(double *arr, int Nx) {
-    double v = arr[0];
-    for (int nx = 0; nx < Nx; nx++) {
-        if (v > arr[nx]) {
-            v = arr[nx];
-        }
-    }
-    return v;
-}
-
-double averarr(double *arr, int Nx) {
-    double v = 0.0;
-    for (int nx = 0; nx < Nx; nx++) {
-        v += arr[nx];
-    }
-    return v / double(Nx);
-}
-
-bool CpuResultCheck(float *a, float *b, float *c, float *rhs, float *old,
-                    int Nx) {
-    if (fabs(b[0] * old[0] + c[0] * old[1] - rhs[0]) > 1.0e-6) {
-        return false;
-    }
-    for (int nx = 1; nx < Nx - 1; nx++) {
-        if (fabs(a[nx] * old[nx - 1] + b[nx] * old[nx] +
-                 c[n] * old[nx + 1] - rhs[nx]) > 1.0e-6) {
-            return false;
-        }
-    }
-    if (fabs(b[Nx - 1] * old[Nx - 1] + a[Nx - 1] * old[Nx - 2] - rhs[Nx - 1]) >
-        1.0e-6) {
-        return false;
-    }
-}
-
-bool GpuResultCheck(float *alist, float *blist, float *clist, float *dlist,
-                    float *xlist, int DMax) {
-    if (fabs(blist[0] * xlist[0] + clist[0] * xlist[1] - dlist[0]) > 1.0e-6) {
-        return false;
-    }
-    for (int nx = 1; nx < DMax - 1; nx++) {
-        if (fabs(alist[nx] * xlist[nx - 1] + blist[nx] * xlist[nx] +
-                 clist[nx] * xlist[nx + 1] - dlist[nx]) > 1.0e-6) {
-            return false;
-        }
-    }
-    if (fabs(blist[Nx - 1] * xlist[Nx - 1] + alist[Nx - 1] * xlist[Nx - 2] -
-             dlist[Nx - 1]) > 1.0e-6) {
-        return false;
-    }
-}
-
-int correctcount(bool *result, int trynumber) {
-    int count = 0;
-    for(int n=0; n<trynumber; n++) {
-        if (result[n]) {
-            count++;
-        }
-    }
-    return count;
-}
+#include "utils.h"
 
 int Test(size_t diagonal_size) {
     std::chrono::time_point<std::chrono::system_clock> tstart, tend;
@@ -98,20 +27,19 @@ int Test(size_t diagonal_size) {
     float *ptr_xlist = thrust::raw_pointer_cast(xlist.data());
 
     size_t Nx = diagonal_size;
-    float *a, *b, *c, *rhs, *old;
-    a = new float[Nx];
-    b = new float[Nx];
-    c = new float[Nx];
-    rhs = new float[Nx];
-    old = new float[Nx];
+    double *a, *b, *c, *rhs, *old;
+    a = new double[Nx];
+    b = new double[Nx];
+    c = new double[Nx];
+    rhs = new double[Nx];
+    old = new double[Nx];
 
     int trynumber = 100;
     double *gputimes = new double[trynumber];
     double *cputimes = new double[trynumber];
-    bool cpuresultcheck = new bool[trynumber];
-    bool gpuresultcheck = new bool[trynumber];
+    bool *cpuresultcheck = new bool[trynumber];
+    bool *gpuresultcheck = new bool[trynumber];
 
-    double v = -1.0;
     for (int count = 0; count < trynumber; count++) {
         for (int i = 0; i < diagonal_size; i++) {
             alist[i] = -1.0 + 0.1 * float(rand()) / float(RAND_MAX);
@@ -126,8 +54,8 @@ int Test(size_t diagonal_size) {
             old[i] = xlist[i];
         }
 
-        a[0] = float(0.0);
-        c[Nx - 1] = float(0.0);
+        a[0] = double(0.0);
+        c[Nx - 1] = double(0.0);
         alist[0] = float(0.0);
         clist[diagonal_size - 1] = float(0.0);
 
@@ -137,31 +65,24 @@ int Test(size_t diagonal_size) {
         duration = tend - tstart;
         gputimes[count] = duration.count();
         gpuresultcheck[count] =
-            ResultCheckGpu(alist, blist, clist, dlist, xlist, diagonal_size);
+            ResultCheckGpu(alist, blist, clist, dlist, xlist, diagonal_size) < 1.0e-5;
 
         tstart = std::chrono::system_clock::now();
         TDMA(a, b, c, rhs, old, Nx);
         tend = std::chrono::system_clock::now();
         duration = tend - tstart;
         cputimes[count] = duration.count();
-        cpuresultcheck[count] = ResultCheckCpu(a, b, c, rhs, old, Nx);
-
-        for (size_t it = 0; it < Nx; it++) {
-            if (fabs(old[it] - xlist[it]) > v) {
-                v = fabs(old[it] - xlist[it]);
-            }
-        }
+        cpuresultcheck[count] = ResultCheckCpu(a, b, c, rhs, old, Nx) < 1.0e-15;
     }
 
     std::cout << "Diagonal size: " << diagonal_size << std::endl;
-    std::cout << "Max deviation: " << v << std::endl;
-    std::cout << "gpu v.s. cpu" << std::endl;
-    std::cout << "correct results: " << correctcount(gpuresultcheck, trynumber) << " <> " << correctcount(cpuresultcheck, trynumber) << std::endl;
-    std::cout << "maximum: " << maxarr(gputimes, trynumber) << " <> "
+    std::cout << "Gpu v.s. Cpu" << std::endl;
+    std::cout << "Correct results: " << correctcount(gpuresultcheck, trynumber) << " <> " << correctcount(cpuresultcheck, trynumber) << std::endl;
+    std::cout << "Maximum time(s): " << maxarr(gputimes, trynumber) << " <> "
               << maxarr(cputimes, trynumber) << std::endl;
-    std::cout << "minimum: " << minarr(gputimes, trynumber) << " <> "
+    std::cout << "Minimum time(s): " << minarr(gputimes, trynumber) << " <> "
               << minarr(cputimes, trynumber) << std::endl;
-    std::cout << "average: " << averarr(gputimes, trynumber) << " <> "
+    std::cout << "Average time(s): " << averarr(gputimes, trynumber) << " <> "
               << averarr(cputimes, trynumber) << std::endl;
     std::cout << std::endl;
 
